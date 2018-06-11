57b21c5f9de3cc47561c5b64d5773ffa7f5daa81
Marco Garzola <zolamarco@gmail.com>
2018-06-11 12:46:34 +0200

    modified comment

--- snapcastHttpd.py
+++ snapcastHttpd.py
@@ -1,9 +1,9 @@
 
 #!/usr/bin/env python
 
+'''
 ##################################################################################
 ## License
-'''
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
@@ -14,10 +14,9 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
-'''
 #
 ##################################################################################
-
+'''
 
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
